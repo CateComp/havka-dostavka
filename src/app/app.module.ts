49c2a0e8555f9980f169468dc7bb645e74272ed5
TopOleh <dgtop997@gmail.com>
2018-10-31 01:29:55 +0200

    Installed ng-bootstrap, fix footer to the bottom.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from 'app/shared/layout/footer/footer.component';
+import { HeaderComponent } from 'app/shared/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
