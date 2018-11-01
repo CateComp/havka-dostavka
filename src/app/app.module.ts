fe921a9dcf4ad6912c41dcdbe1631cc7f43f4538
Ostap <ostap08@gmail.com>
2018-11-01 13:51:52 +0200

    made nav and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from 'app/shared/layout/footer/footer.component';
 import { HeaderComponent } from 'app/shared/layout/header/header.component';
+import { HomeComponent } from './modules/home/home.component';
+import { MenuComponent } from './modules/menu/menu.component';
+import { NewsComponent } from './modules/news/news.component';
+import { DeliveryComponent } from './modules/delivery/delivery.component';
+import { TrackingComponent } from './modules/tracking/tracking.component';
+import { AboutUsComponent } from './modules/about-us/about-us.component';
+import { CartComponent } from './modules/cart/cart.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'delivery', component: DeliveryComponent },
+  { path: 'tracking', component: TrackingComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'cart', component: CartComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    MenuComponent,
+    NewsComponent,
+    DeliveryComponent,
+    TrackingComponent,
+    AboutUsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()// add routes
+    NgbModule.forRoot(),// add routes
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
