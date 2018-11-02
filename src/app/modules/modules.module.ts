45f30e79fd544452de151914c5b9014a44ffcc70
Oleh Topchak <Oleh_Topchak@epam.com>
2018-11-02 21:34:54 +0200

    [US-2-3] Menu-component-wrapper-item

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -8,15 +8,35 @@ import { TrackingComponent } from './tracking/tracking.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from 'app/modules/home/home.component';
 import { CartComponent } from './cart/cart.component';
+//shared
+import { MenuWrapperComponent } from 'app/shared/menu-wrapper/menu-wrapper.component';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
 
+//services
+import { DishServiceService } from 'app/core/services/dish-service.service'
+// help modules
 import { AppRoutingModule } from 'app/core/app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { from } from 'rxjs';
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  declarations: [MenuComponent, NewsComponent, DeliveryComponent, TrackingComponent, AboutComponent, CartComponent, HomeComponent],
+  declarations: [
+    MenuComponent,
+    NewsComponent,
+    DeliveryComponent,
+    TrackingComponent,
+    AboutComponent,
+    CartComponent,
+    HomeComponent,
+    MenuWrapperComponent,
+    MenuItemComponent
+  ],
+  providers: [DishServiceService],
   exports: [MenuComponent, NewsComponent, DeliveryComponent, TrackingComponent, AboutComponent, CartComponent, HomeComponent]
 })
 export class ModulesModule { }
