8d822188a0e7a96cf6e9f924b9cb959f0e61703b
volodblack <cherk.v8@gmail.com>
2018-11-07 19:15:04 +0200

    add news responsive page with data from json and some styles

--- src/app/modules/news/news.component.ts
+++ src/app/modules/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'app/core/services/news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  public news = [];
+  public currentUrl;
+  public limitItems: number;
+
+  constructor(private _newsService: NewsService, private _router: Router) {
+    _router.events.subscribe((url: any) => this.currentUrl = url);
+  }
 
   ngOnInit() {
+    this._newsService.getNews()
+      .subscribe(data => this.news = data);
   }
 
 }
