a9853b1c069137af8bf5ae144c91a1478b43eb33
Ostap <ostap08@gmail.com>
2018-11-07 23:13:36 +0200

    added login with google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { ModulesModule } from './modules/modules.module';
 // Imported firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { FirebaseService } from 'app/core/services/firebase.service'
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FirebaseService } from 'app/core/services/firebase.service';
 
 import { environment } from '../environments/environment';
 
@@ -23,6 +24,7 @@ import { environment } from '../environments/environment';
     ModulesModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [FirebaseService],
   bootstrap: [AppComponent]
--- src/app/modules/menu/menu.component.ts
+++ src/app/modules/menu/menu.component.ts
@@ -15,5 +15,4 @@ export class MenuComponent implements OnInit {
       this.menu = items;
     })
   }
-
 }
