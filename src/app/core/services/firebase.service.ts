9c7dd891cda463f260073c48f1df46832f1bd7ad
volodblack <cherk.v8@gmail.com>
2018-11-12 14:43:47 +0200

    about-us, data from firebase

--- src/app/core/services/firebase.service.ts
+++ src/app/core/services/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs/internal/Observable';
 import { Dish } from 'app/core/interfaces/dish';
+import { AboutUs } from 'app/core/interfaces/about-us';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -21,6 +22,20 @@ export class FirebaseService {
           return data.map(e => {
             const id = e.payload.doc.id;
             const dataObj = e.payload.doc.data() as Dish;
+            return { id, ...dataObj };
+          });
+        })
+      );
+  }
+
+  getAboutUs(): Observable<AboutUs[]> {
+    return this.afs.collection<AboutUs>('about-us')
+    .snapshotChanges()
+      .pipe(
+        map(data => {
+          return data.map(e => {
+            const id = e.payload.doc.id;
+            const dataObj = e.payload.doc.data() as AboutUs;
             return { id, ...dataObj };
           });
         })
