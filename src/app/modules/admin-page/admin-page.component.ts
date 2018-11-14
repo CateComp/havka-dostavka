13918a726a3151852f380495302fc29e59e62288
Oleh Topchak <Oleh_Topchak@epam.com>
2018-11-14 20:22:17 +0200

    Added posible to add dish

--- src/app/modules/admin-page/admin-page.component.ts
+++ src/app/modules/admin-page/admin-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from 'app/core/services/firebase.service';
 import { Dish } from 'app/core/interfaces/dish';
+import { HttpClient } from '@angular/common/http';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-admin-page',
@@ -8,14 +10,15 @@ import { Dish } from 'app/core/interfaces/dish';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent implements OnInit {
+  public image;
   public dish: Dish = {
-    name: 'custom',
-    type: 'custom',
+    name: '',
+    type: '',
     todaymenu: false,
-    price: 100,
-    weight: 'custom',
-    img: 'custom',
-    info: 'custom'
+    price: 0,
+    weight: '',
+    img: '',
+    info: ''
   };
 
   public filterProp = [
@@ -33,14 +36,31 @@ export class AdminPageComponent implements OnInit {
     isChecked: false}
   ];
 
-  constructor(private _fbs: FirebaseService) { }
+  constructor(private _fbs: FirebaseService, private _http: HttpClient) { }
 
   addDish(dish: Dish) {
     this._fbs.addDish(dish);
   }
+  addImage(event) {
+    this.dish.img = event.target.files[0];
+    console.log("dish", this.dish.img)
+  }
 
   submitDish() {
+    console.log(this.dish);
+    // this._fbs.addDish(this.dish);
+  }
+  onUpload() {
+    console.log('Upload', this.dish.img)
+    let database = firebase.database()
+    let storage = firebase.storage();
+    let randId = Math.random();
+    let buildenavn = storage.ref("images-mocks/" + randId);
+    buildenavn.put(this.dish.img);
 
+    this.dish.img = "https://firebasestorage.googleapis.com/v0/b/havka-2726f.appspot.com/o/images-mocks%2F" + randId + "?alt=media";
+    console.log(this.dish.img)
+    this._fbs.addDish(this.dish);
   }
 
   submitValidation() {
